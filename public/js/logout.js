0b6b23184d73138381d9ff435b269ac6bb6ec002
D0KD0 <113081917+D0KD0@users.noreply.github.com>
2023-01-05 19:24:00 -0800

    logout delete

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,5 +1,5 @@
 const logout = async () => {
-    const response = await fetch('/api/users/logout', {
+    const response = await fetch('/api/users', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
     });
@@ -13,3 +13,4 @@ const logout = async () => {
   
   document.querySelector('#logout').addEventListener('click', logout);
   
+// logout handlebar is not needed
