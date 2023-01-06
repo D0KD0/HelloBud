98e259dda7cf2fc2bd444bea02d9fabbe89f8967
vantam8300 <vantam8300@gmail.com>
2023-01-05 22:12:49 -0800

    finish dispensary map

--- public/js/map.js
+++ public/js/map.js
@@ -6,8 +6,8 @@ mapboxgl.accessToken =
 var map = new mapboxgl.Map({
     container: "map",
     style: "mapbox://styles/mapbox/light-v11",
-    center: [-77.034084142948, 38.909671288923],
-    zoom: 10,
+    center: [-117.863899, 33.706684],
+    zoom: 13,
 });
 
 let dispensaries;
@@ -16,246 +16,6 @@ fetch("/api/dispensaries").then(
         res.json().then(
             data => {
                 dispensaries = data
-                console.log(dispensaries)
-                const stores = {
-                    "features": [
-                        {
-                            "geometry": {
-                                "type": "Point",
-                                "coordinates": [
-                                    -77.034084142948,
-                                    38.909671288923
-                                ]
-                            },
-                            "properties": {
-                                "phoneFormatted": "(202) 234-7336",
-                                "phone": "2022347336",
-                                "address": "1471 P St NW",
-                                "city": "Washington DC",
-                                "country": "United States",
-                                "postalCode": "20005",
-                                "state": "D.C."
-                            }
-                        },
-                        {
-
-                            "geometry": {
-                                "type": "Point",
-                                "coordinates": [
-                                    -77.049766,
-                                    38.900772
-                                ]
-                            },
-                            "properties": {
-                                "phoneFormatted": "(202) 507-8357",
-                                "phone": "2025078357",
-                                "address": "2221 I St NW",
-                                "city": "Washington DC",
-                                "country": "United States",
-                                "postalCode": "20037",
-                                "state": "D.C."
-                            }
-                        },
-                        {
-
-                            "geometry": {
-                                "type": "Point",
-                                "coordinates": [
-                                    -77.043929,
-                                    38.910525
-                                ]
-                            },
-                            "properties": {
-                                "phoneFormatted": "(202) 387-9338",
-                                "phone": "2023879338",
-                                "address": "1512 Connecticut Ave NW",
-                                "city": "Washington DC",
-                                "country": "United States",
-                                "postalCode": "20036",
-                                "state": "D.C."
-                            }
-                        },
-                        {
-
-                            "geometry": {
-                                "type": "Point",
-                                "coordinates": [
-                                    -77.0672,
-                                    38.90516896
-                                ]
-                            },
-                            "properties": {
-                                "phoneFormatted": "(202) 337-9338",
-                                "phone": "2023379338",
-                                "address": "3333 M St NW",
-                                "city": "Washington DC",
-                                "country": "United States",
-                                "postalCode": "20007",
-                                "state": "D.C."
-                            }
-                        },
-                        {
-
-                            "geometry": {
-                                "type": "Point",
-                                "coordinates": [
-                                    -77.002583742142,
-                                    38.887041080933
-                                ]
-                            },
-                            "properties": {
-                                "phoneFormatted": "(202) 547-9338",
-                                "phone": "2025479338",
-                                "address": "221 Pennsylvania Ave SE",
-                                "city": "Washington DC",
-                                "country": "United States",
-                                "postalCode": "20003",
-                                "state": "D.C."
-                            }
-                        },
-                        {
-
-                            "geometry": {
-                                "type": "Point",
-                                "coordinates": [
-                                    -76.933492720127,
-                                    38.99225245786
-                                ]
-                            },
-                            "properties": {
-                                "address": "8204 Baltimore Ave",
-                                "city": "College Park",
-                                "country": "United States",
-                                "postalCode": "20740",
-                                "state": "MD"
-                            }
-                        },
-                        {
-
-                            "geometry": {
-                                "type": "Point",
-                                "coordinates": [
-                                    -77.097083330154,
-                                    38.980979
-                                ]
-                            },
-                            "properties": {
-                                "phoneFormatted": "(301) 654-7336",
-                                "phone": "3016547336",
-                                "address": "4831 Bethesda Ave",
-                                "cc": "US",
-                                "city": "Bethesda",
-                                "country": "United States",
-                                "postalCode": "20814",
-                                "state": "MD"
-                            }
-                        },
-                        {
-
-                            "geometry": {
-                                "type": "Point",
-                                "coordinates": [
-                                    -77.359425054188,
-                                    38.958058116661
-                                ]
-                            },
-                            "properties": {
-                                "phoneFormatted": "(571) 203-0082",
-                                "phone": "5712030082",
-                                "address": "11935 Democracy Dr",
-                                "city": "Reston",
-                                "country": "United States",
-                                "postalCode": "20190",
-                                "state": "VA"
-                            }
-                        },
-                        {
-
-                            "geometry": {
-                                "type": "Point",
-                                "coordinates": [
-                                    -77.10853099823,
-                                    38.880100922392
-                                ]
-                            },
-                            "properties": {
-                                "phoneFormatted": "(703) 522-2016",
-                                "phone": "7035222016",
-                                "address": "4075 Wilson Blvd",
-                                "city": "Arlington",
-                                "country": "United States",
-                                "postalCode": "22203",
-                                "state": "VA"
-                            }
-                        },
-                        {
-
-                            "geometry": {
-                                "type": "Point",
-                                "coordinates": [
-                                    -75.28784,
-                                    40.008008
-                                ]
-                            },
-                            "properties": {
-                                "phoneFormatted": "(610) 642-9400",
-                                "phone": "6106429400",
-                                "address": "68 Coulter Ave",
-                                "city": "Ardmore",
-                                "country": "United States",
-                                "postalCode": "19003",
-                                "state": "PA"
-                            }
-                        },
-                        {
-
-                            "geometry": {
-                                "type": "Point",
-                                "coordinates": [
-                                    -75.20121216774,
-                                    39.954030175164
-                                ]
-                            },
-                            "properties": {
-                                "phoneFormatted": "(215) 386-1365",
-                                "phone": "2153861365",
-                                "address": "3925 Walnut St",
-                                "city": "Philadelphia",
-                                "country": "United States",
-                                "postalCode": "19104",
-                                "state": "PA"
-                            }
-                        },
-                        {
-                            "geometry": {
-                                "type": "Point",
-                                "coordinates": [
-                                    -77.043959498405,
-                                    38.903883387232
-                                ]
-                            },
-                            "properties": {
-                                "phoneFormatted": "(202) 331-3355",
-                                "phone": "2023313355",
-                                "address": "1901 L St. NW",
-                                "city": "Washington DC",
-                                "country": "United States",
-                                "postalCode": "20036",
-                                "state": "D.C."
-                            }
-                        }
-                    ]
-                };
-
-                /**
-                * Assign a unique id to each store. You'll use this `id`
-                * later to associate each point on the map with a listing
-                * in the sidebar.
-                */
-                stores.features.forEach((store, i) => {
-                    store.properties.id = i;
-                });
-
                 /**
                 * Wait until the map loads to make changes to the map.
                 */
@@ -345,14 +105,21 @@ fetch("/api/dispensaries").then(
                         link.href = '#';
                         link.className = 'title';
                         link.id = `link-${dispensary.id}`;
-                        link.innerHTML = `${dispensary.address}`;
+                        link.innerHTML = `${dispensary.name}`;
 
                         /* Add details to the individual listing. */
                         const details = listing.appendChild(document.createElement('div'));
-                        details.innerHTML = `${dispensary.city}`;
+                        details.className = 'my-2';
+                        
                         if (dispensary.phone) {
-                            details.innerHTML += ` &middot; ${dispensary.phone}`;
+                            details.innerHTML += `Phone Number: ${dispensary.phone}`;
                         }
+
+                        const menuBtn = listing.appendChild(document.createElement('a'));
+                        menuBtn.href = '#';
+                        menuBtn.className = 'btn btn-secondary rounded d-block';
+                        menuBtn.id = `menu-${dispensary.id}`;
+                        menuBtn.innerHTML = `View Menu`;
 
                         /**
                         * Listen to the element and when it is clicked, do four things:
@@ -382,7 +149,6 @@ fetch("/api/dispensaries").then(
                 * a given center point.
                 **/
                 function flyToStore(dispensary) {
-                    console.log(dispensary.point.coordinates)
                     map.flyTo({
                         center: dispensary.point.coordinates,
                         zoom: 15
@@ -398,7 +164,7 @@ fetch("/api/dispensaries").then(
                     const popup = new mapboxgl.Popup({ closeOnClick: false })
                         .setLngLat(dispensary.point.coordinates)
                         .setHTML(
-                            `<h3>Sweetgreen</h3><h4>${dispensary.address}</h4>`
+                            `<h3>${dispensary.name}</h3><h4>${dispensary.address}, ${dispensary.city}, ${dispensary.state} ${dispensary.zip_code}</h4>`
                         )
                         .addTo(map);
                 }
