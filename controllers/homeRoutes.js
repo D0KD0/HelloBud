148c04c6702db54096178d4a16f429dcbddc71fa
vantam8300 <vantam8300@gmail.com>
2023-01-07 20:54:01 -0800

    creating product page

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,13 +1,38 @@
 const router = require('express').Router();
 
-const { Dispensary } = require('../models');
+const { Product, Category, Dispensary, Brand } = require('../models');
 
 router.get("/", async (req, res) => {
     res.render("homepage")
 })
 
 router.get("/products", async (req, res) => {
-    res.render("products")
+    try {
+        const productData = await Product.findAll({
+            include: [
+                {
+                  model: Brand,
+                  attributes: ['name'],
+                },
+                {
+                    model: Category,
+                    attributes: ['name'],
+                },
+                {
+                    model: Dispensary,
+                    attributes: ['name'],
+                }
+              ],
+        });
+
+        const categoryData = await Category.findAll({ });
+
+        const products = productData.map((product) => product.get({ plain: true }));
+        const categories = categoryData.map((category) => category.get({ plain: true }));
+        res.render("products", {categories, products, logged_in: req.session.logged_in });
+    } catch (err) {
+        res.status(500).json(err);
+    }
 })
 
 router.get("/dispensary", async (req, res) => {
