ccf88a97b1ff8f05bdcae3a8671d334c333d2e0c
vantam8300 <vantam8300@gmail.com>
2023-01-09 18:09:40 -0800

    creating homepage

---
+++ controllers/productRoutes.js
@@ -0,0 +1,7 @@
+const router = require('express').Router();
+const _ = require('lodash/core');
+const { Product, Category, Dispensary, Brand } = require('../models');
+
+
+
+module.exports = router;
